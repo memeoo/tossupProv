6c5748f17a1935eff3c5b49fce631c9922a04bcd
memeSong <meme@memeSongui-MacBook-Pro.local>
2019-03-04 09:13:07 +0900

    Login and Sign up UI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,77 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Form, FormGroup, Label, Input, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false
+    }
+    this.toggle = this.toggle.bind(this)
+
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div style={{marginTop:"33px"}}>
+          <span className="mainTitle">Toss Up!</span><span style={{marginLeft:"12px",fontSize:"18px"}}>for examiner</span>
+        </div>
+        <div className="form">
+          <Form>
+            <FormGroup>
+              <Label for="id">ID</Label>
+              <Input type="text" name="id" id="id" maxLength="20" />
+            </FormGroup>
+            <FormGroup>
+              <Label>PASSWORD</Label>
+              <Input type="password" name="pass" />
+            </FormGroup>
+          </Form>
+
+          <Button color="primary" size="lg" block >로그인 (Login)</Button>
+          <Button outline color="secondary" size="sm" block style={{ marginTop: "12px" }} onClick={this.toggle}>회원 가입(Sign up)</Button>
+        </div>
+        <div>
+          <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
+            <ModalHeader toggle={this.toggle}>Membership (as an examiner)</ModalHeader>
+            <ModalBody>
+              <Form>
+                <FormGroup>
+                  <Label for="id">ID</Label>
+                  <Input type="text" name="id" id="id" maxLength="20" />
+                </FormGroup>
+                <FormGroup>
+                  <Label>PASSWORD</Label>
+                  <Input type="password" name="pass" />
+                </FormGroup>
+                <FormGroup>
+                  <Label>Repeat Password</Label>
+                  <Input type="password" name="pass_re" />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="id">Email</Label>
+                  <Input type="email" name="mail" id="emaile" maxLength="35" />
+                </FormGroup>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button color="primary" onClick={this.toggle}>Submit</Button>{' '}
+              <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+            </ModalFooter>
+          </Modal>
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
